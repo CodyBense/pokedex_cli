1b4be5682e4553640bd63024e44bd3a5dfb1558b
CodyBense <codybense@gmail.com>
2024-05-30 21:21:45 -0400

    build simple repl

--- main.go
+++ main.go
@@ -2,8 +2,35 @@ package main
 
 import (
     "fmt"
+    "bufio"
+    "strings"
+    "os"
 )
 
 func main() {
-    fmt.Println("Hello, world")
+    // Hardcoded commands
+    commands := map[string]interface{} {
+        "help": displayHelp,
+        "clear": clearScreen,
+    }
+
+    // Begin repl loop
+    reader := bufio.NewScanner(os.Stdin)
+    printPrompt()
+    for reader.Scan() {
+        text := cleanInput(reader.Text())
+        if command, exists := commands[text]; exists {
+            // call hardcoded function
+            command.(func())()
+        } else if strings.EqualFold("exit", text) {
+            // close program on exit
+            return
+        } else {
+            // pass command to parser
+            handleCmd(text)
+        }
+        printPrompt()
+    }
+    // Print and aditional line if we encounter an EOF character
+    fmt.Println()
 }
--- repl.go
+++ repl.go
@@ -1,24 +1,55 @@
 package main
 
 import (
-    "errors"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
 )
 
-type cliCommand struct {
-    name string
-    description string
-    callback func() error
+var cliName string = "pokdex"
+
+// Prints prompt for every loop
+func printPrompt() {
+    fmt.Println(cliName, "> ")
 }
 
-return map[string]cliCommand{
-    "help": {
-        name: "help",
-        description: "Displays a help message",
-        callBack: commandHelp,
-    },
-    "exit": {
-        name: "exit",
-        description: "Exit the pokedex",
-        callBack: commandExit,
-    },
+// Prints for unkown functions
+func printUnknown(text string) {
+    fmt.Println(text, ": command not found")
 }
+
+// Prints all hardcoded functions
+func displayHelp() {
+    fmt.Printf(
+        "Welcome to %v! These are the avilable commands: \n", cliName,
+    )
+    fmt.Println("help  - Show available commands")
+    fmt.Println("clear - Clear the terminal screen")
+    fmt.Println("exit  - Exit your connection")
+}
+
+// Clears the screen
+func clearScreen() {
+    cmd := exec.Command("clear")
+    cmd.Stdout = os.Stdout
+    cmd.Run()
+}
+
+// attempts to recover from a bad command
+func handleInvalidCommand(text string) {
+    defer printUnknown(text)
+}
+
+// parses the given command
+func handleCmd(text string) {
+    handleInvalidCommand(text)
+}
+
+// helper function to cleanup input, removes white space and converts to lowercase
+func cleanInput(text string) string {
+    output := strings.TrimSpace(text)
+    output = strings.ToLower(output)
+    return output
+}
+
