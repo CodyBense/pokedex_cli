670f2f5d118db9345e2201f393abac19a6d0a3cb
CodyBense <codybense@gmail.com>
2024-06-12 17:02:34 -0400

    fixing errors

--- internal/pokeapi/location_get.go
+++ internal/pokeapi/location_get.go
@@ -6,40 +6,42 @@ import(
     "net/http"
 )
 
-func (c *Client) GetLocaiton(locationName string) (Location, error) {
-    url := baseURL + "/location-area/" + locationName
+// GetLocation -
+func (c *Client) GetLocation(locationName string) (Location, error) {
+	url := baseURL + "/location-area/" + locationName
 
-    if val, ok := c.cache.Get(url); ok {
-        locationResp := Location{}
-        err := json.Unmarshal(val, &locationResp)
-        if err != nil {
-            return Location{}, err
-        }
-        return locationResp, nil
-    }
+	if val, ok := c.cache.Get(url); ok {
+		locationResp := Location{}
+		err := json.Unmarshal(val, &locationResp)
+		if err != nil {
+			return Location{}, err
+		}
+		return locationResp, nil
+	}
 
-    req, err := http.NewRequest("GET", url, nil)
-    if err != nil {
-        return Location{}, err
-    }
-    resp, err := c.httpClient.Do(req)
-    if err != nil {
-        return Location{}, err
-    }
-    defer resp.Body.Close()
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return Location{}, err
+	}
 
-    dat, err := io.ReadAll(resp.Body)
-    if err != nil {
-        return Location{}, err
-    }
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return Location{}, err
+	}
+	defer resp.Body.Close()
 
-    locationResp := Location{}
-    err = json.Unmarshal(dat, &locationResp)
-    if err != nil {
-        return Location{}, err
-    }
+	dat, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return Location{}, err
+	}
 
-    c.cache.Add(url, dat)
+	locationResp := Location{}
+	err = json.Unmarshal(dat, &locationResp)
+	if err != nil {
+		return Location{}, err
+	}
 
-    return locationResp, nil
+	c.cache.Add(url, dat)
+
+	return locationResp, nil
 }
